4164257ff693b9cf3e3b84c12db99f826e3d7da6
Bryan Tuttle <b-tuttle@users.noreply.github.com>
2017-12-14 15:52:02 -0800

    Initial commit with basic functionality for tracking one team

---
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.android.courtcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    int scoreTeamA = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int scoreTeamA) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(scoreTeamA));
+    }
+
+    public void threePointerA (View v) {
+        scoreTeamA += 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void twoPointerA (View v) {
+        scoreTeamA += 2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void freeThrowA (View v) {
+        scoreTeamA += 1;
+        displayForTeamA(scoreTeamA);
+    }
+}
